bde1e9b90c7c56aa1ab72e89e7d8665b308e4046
Alena Kamenská <alekame019@pslib.cz>
2020-10-04 08:50:37 +0200

    Přidejte soubory projektu.

---
+++ Kamenska_DU/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kamenska_DU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
